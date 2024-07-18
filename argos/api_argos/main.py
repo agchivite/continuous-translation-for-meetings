12f5d5bf6904e8026f49a53e0c68a7c0ef0b14f4
Angel M.Chivite <sbytmacke@gmail.com>
2024-07-18 10:21:45 +0200

    [Add] Support all google languages

--- argos/api_argos/main.py
+++ argos/api_argos/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query  # type: ignore
-from googletrans import Translator  # type: ignore
+from googletrans import Translator, LANGUAGES  # type: ignore
+import uvicorn  # type: ignore
 
 app = FastAPI()
 translator = Translator()
@@ -12,29 +13,55 @@ def root():
             {
                 "path": "/translate",
                 "method": "GET",
-                "description": "Translate text from English to Vietnamese",
+                "description": "Translate text to the specified language",
                 "parameters": [
                     {
                         "name": "text",
                         "type": "str",
                         "description": "Text to be translated",
                         "required": True,
-                    }
+                    },
+                    {
+                        "name": "lang",
+                        "type": "str",
+                        "description": "Target language code",
+                        "required": True,
+                    },
                 ],
-            }
+            },
+            {
+                "path": "/languages",
+                "method": "GET",
+                "description": "Get a list of supported languages",
+            },
         ]
     }
 
 
-@app.get("/translate")
-def translate_text(text: str = Query(..., description="Text to translate")):
-    target_lang = "vi"
+@app.get("/translate/{lang}")
+def translate_text(lang: str, text: str = Query(..., description="Text to translate")):
     try:
-        translation = translator.translate(text, dest=target_lang)
+        # Default
+        short_lang = "es"
+
+        if len(lang) == 2:
+            short_lang = lang
+        else:
+            short_lang = lang.split("-")[0]
+
+        if short_lang not in LANGUAGES:
+            return {"error": f"Language '{short_lang}' is not supported"}
+
+        translation = translator.translate(text, dest=short_lang)
         return {"translated_text": translation.text}
     except Exception as e:
         return {"error": f"Translation error: {str(e)}"}
 
 
+@app.get("/languages")
+def get_languages():
+    return {"languages": LANGUAGES}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8089)  # type: ignore
+    uvicorn.run(app, host="0.0.0.0", port=8089)
